ec33a66b58c9b7c1fb4fb042da1ea0a39564c273
AloeLavi <story_teller@bk.ru>
2022-10-02 21:21:33 +0300

    simple parametrized test

--- src/test/java/quru/qa/SimpleJunitExample.java
+++ src/test/java/quru/qa/SimpleJunitExample.java
@@ -2,6 +2,7 @@ package quru.qa;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -23,25 +24,26 @@ public class SimpleJunitExample {
 
     }
 
+    @DisplayName("Проверка подбора билетов на самолет из Москвы в Пермь")
     @Test
     void tutuSimpleTest(){
         open("https://www.tutu.ru/");
         $(byName("city_from")).setValue("Москва").pressEnter();
-        $(byName("city_to")).setValue("Казань").pressEnter();
+        $(byName("city_to")).setValue("Пермь").pressEnter();
         $(byName("date_from")).setValue("01.01.2023").pressEnter();
         $(byClassName("j-submit_button")).click();
-        $(byClassName("Ibn0I0o1tdeDjL4-YTn1n")).shouldHave(text("Москва — Казань"));
+        $(byClassName("Ibn0I0o1tdeDjL4-YTn1n")).shouldHave(text("Москва — Пермь"));
     }
 
-    @ValueSource
-    @ParameterizedTest
-    void tutuParameterizedTest(){
+    @ValueSource(strings = {"Казань", "Сочи"})
+    @ParameterizedTest(name = "Проверка подбора билетов на самолет из Москвы в {0}")
+    void tutuParameterizedTest(String testData){
         open("https://www.tutu.ru/");
         $(byName("city_from")).setValue("Москва").pressEnter();
-        $(byName("city_to")).setValue("Казань").pressEnter();
+        $(byName("city_to")).setValue(testData).pressEnter();
         $(byName("date_from")).setValue("01.01.2023").pressEnter();
         $(byClassName("j-submit_button")).click();
-        $(byClassName("Ibn0I0o1tdeDjL4-YTn1n")).shouldHave(text("Москва — Казань"));
+        $(byClassName("Ibn0I0o1tdeDjL4-YTn1n")).shouldHave(text("Москва — "+ testData));
     }
 
 }
